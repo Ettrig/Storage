b7ffff96a6997573795dcfdaccde10729ca06034
Ettrig <rolf.engstrand@gmail.com>
2019-05-20 13:39:36 +0200

    Add project files.

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id {get; set;}
+        public string Name {get; set;}
+        [Range(1,1000000)]
+        public int Price {get; set;}
+        [DataType (DataType.Date)]
+        public DateTime Orderdate {get; set;}
+        [Required]
+        [Display(Name ="Kategori")]
+        public string Category {get; set;}
+        public string Shelf {get; set;}
+        [Range(0, 1000)]
+        public int Count {get; set;}
+        public string Description {get; set;}
+    }
+}
